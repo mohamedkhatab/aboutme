d915588de137e1fcb1e89e3beae4a69c9d00a77b
mohamed khatab <hamosuper8@gmail.com>
2017-06-30 21:57:13 +0200

    Delete ass.js

--- ass.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $(".head").slideDown(500),
-$(".story").slideDown(600),
-$(".contact").animate({height: '300px', opacity: '0.4'}, "slow");
-    });
-});
