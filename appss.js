3290bdc53b510f92bb0b0a9184cdcb3c547c7287
mohamed khatab <hamosuper8@gmail.com>
2017-07-18 23:09:49 +0200

    Add files via upload

---
+++ appss.js
@@ -0,0 +1,25 @@
+var lolzz = document.getElementById("lolz");
+
+function sss(){
+
+   
+    if (lolzz.value == "hello"){
+alert("it is KHATAB");
+}
+    
+};
+
+function ssss(){
+
+    var result=document.getElementsByClassName("result")[0];
+ result.style.backgroundColor="red";
+    
+    
+};
+
+function ssssss(){
+
+    var result=document.getElementsByClassName("result")[0];
+ result.style.backgroundColor="greenyellow";
+   
+};
